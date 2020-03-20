69ff38aa75d0755700bf76761fceaa60e70c9a69
iloveket <davidrb2017@outlook.com>
2020-03-20 19:27:54 +0000

    .env file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Search from './components/Search';
 
+require('dotenv').config();
+
+const apiurl = process.env.REACT_APP_API_KEY;
+
 function App() {
   return (
     <div className="App">
