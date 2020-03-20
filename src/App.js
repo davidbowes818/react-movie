a8a9482dd1ccd1de05ce9cd3c0eda2659162d694
iloveket <davidrb2017@outlook.com>
2020-03-20 19:38:05 +0000

    Handle input method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/Search';
 
 require('dotenv').config();
 
-const apiurl = process.env.REACT_APP_API_KEY;
-
 function App() {
+  const [state, setState] = useState({
+    s: '',
+    results: [],
+    selected: {}
+  });
+
+  const apiurl = process.env.REACT_APP_API_KEY;
+
+  const handleInput = e => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s };
+    });
+
+    console.log(state.s);
+  };
   return (
     <div className="App">
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
